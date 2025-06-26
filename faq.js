8c03bb12f1c45545a2504a4550e180fd4356509e
Burzah <116982774+Burzah@users.noreply.github.com>
2025-06-26 06:22:27 -0700

    cleanup and adjustments

--- faq.js
+++ faq.js
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const faqItem = button.parentElement;
+        faqItem.classList.toggle('active');
+
+        document.querySelectorAll('.faq-item').forEach(item => {
+            if (item !== faqItem) {
+                item.classList.remove('active');
+            }
+        });
+    });
+});
